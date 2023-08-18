0d1d825483c524f90fc7f3dd585ac9f9afea6167
isaac20042012 <sandraeigor@gmail.com>
2023-08-18 10:09:12 -0300

    :sparkles: Switching to my data

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 function toggleMode() {
     const html = document.documentElement
     html.classList.toggle('light')
-
-    const img = document.querySelector('#profile img')
-    if (html.classList.contains('light')) {
-        img.setAttribute('src', './assets/avatar-light.png')
-        img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos e jaqueta preta, sem barba e fundo gradiente do roxo para o azul')
-    } else {
-        img.setAttribute('src', './assets/avatar.png')
-        img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo')
-    }
 }
